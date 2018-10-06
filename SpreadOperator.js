e264ec7033b6d66207b4ef35d9180b15ac4d6ca4
Sanket Apte <sanket1106@gmail.com>
2018-10-06 16:58:34 +0530

    Create SpreadOperator.js

---
+++ SpreadOperator.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4];
+console.log(newNumbers);
+
+
+const person = {
+  name: "max"
+};
+
+const newPerson = {
+  ...person,
+  age:28
+}
+
+console.log(newPerson);
