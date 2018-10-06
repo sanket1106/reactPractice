92ff7aefa2d6db000be4cd5bbe8a464be460277e
Sanket Apte <sanket1106@gmail.com>
2018-10-06 17:12:38 +0530

    Create Destructuring.js

---
+++ Destructuring.js
@@ -0,0 +1,13 @@
+// Array Destructuring
+[a,b] = ['Hello', 'Sanket'];
+console.log(a);
+console.log(b);
+
+const numbers = [1, 2, 3];
+[num1, num2, num3] = numbers;
+console.log(num1, num3);
+
+// Object Destructuring
+//{name, age} = {name:'max', age:28};
+//console.log(name);
+//console.log(age);
